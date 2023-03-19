f48d4d6b78098fcd0904cb8fc52c4fba20a64d1f
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-03-19 11:59:54 -0300

    feat(Pessoa): :art: added use cases modular export

--- Atividades/atividade-pratica-01/src/application/use-cases/pessoa/index.ts
+++ Atividades/atividade-pratica-01/src/application/use-cases/pessoa/index.ts
@@ -1,1 +1,5 @@
+export * from "./delete-pessoa-use-case";
+export * from "./edit-pessoa-use-case";
+export * from "./get-all-pessoa-use-case";
+export * from "./get-pessoa-use-case";
 export * from "./register-pessoa-use-case";
