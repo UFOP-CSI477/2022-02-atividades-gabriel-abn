b5f55bd840271b7585f053ab1bcdee1fec25d45c
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-03-18 23:56:56 -0300

    feat(LocalColeta): :sparkles: added create and findAll methods

--- Atividades/atividade-pratica-01/src/application/repositories/local-coleta-repository.ts
+++ Atividades/atividade-pratica-01/src/application/repositories/local-coleta-repository.ts
@@ -1,5 +1,7 @@
-import { LocaisColetaProps } from "@domain/local-coleta";
+import { LocalColeta, LocalColetaProps } from "@domain/local-coleta";
 
 export interface ILocalColetaRepository {
-  findById(id: string): Promise<LocaisColetaProps>;
+  create(localColeta: LocalColeta): Promise<LocalColetaProps>;
+  findById(id: string): Promise<LocalColetaProps>;
+  findAll(): Promise<LocalColetaProps[]>;
 }
