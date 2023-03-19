2b83dabdd54e7c9c4660a31c5535df41713bde0b
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-03-19 13:56:14 -0300

    feat(LocalColeta): :art: created modular exports

---
+++ Atividades/atividade-pratica-01/src/presentation/controllers/locais-coleta/index.ts
@@ -0,0 +1,5 @@
+export * from "./create-local-coleta-controller";
+export * from "./delete-local-coleta-controller";
+export * from "./get-all-local-coleta-controller";
+export * from "./get-local-coleta-controller";
+export * from "./update-local-coleta-controller";
