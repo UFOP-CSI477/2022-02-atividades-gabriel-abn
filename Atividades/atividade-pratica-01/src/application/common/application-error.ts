02bef8fcac1632943f4c7a911a555377bde9c209
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-03-19 11:58:45 -0300

    feat: :art: added toString method to base application error class

--- Atividades/atividade-pratica-01/src/application/common/application-error.ts
+++ Atividades/atividade-pratica-01/src/application/common/application-error.ts
@@ -1,5 +1,10 @@
 export class ApplicationError extends Error {
   constructor(message: string) {
     super(message);
+    this.name = "Application Error";
+  }
+
+  public toString(): { message: string; name: string } {
+    return { message: this.message, name: this.name };
   }
 }
